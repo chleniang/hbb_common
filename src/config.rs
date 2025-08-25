b6c15b3791d5ec5bd63baca236fd7266011a8390
chleniang <42363924+chleniang@users.noreply.github.com>
2025-08-25 10:56:58 +0800

    Update config.rs1

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,10 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["125.122.37.107"];
+pub const RS_PUB_KEY: &str = "CC38039202F0F2H37BD2JXWLD068M59C8D332E15DBDAF260_aueV3p6nx32jk90";
+// pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
+// pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
